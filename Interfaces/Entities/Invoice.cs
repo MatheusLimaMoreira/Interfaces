00c29f65c96940d7a8bbb199dd7da5d7b625ecd4
Matheus de Lima <matheus.moreira127@gmail.com>
2025-07-01 21:58:11 -0300

    Criação do projeto de interfaces

---
+++ Interfaces/Entities/Invoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Entities
+{
+    internal class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return BasicPayment.ToString("F2", System.Globalization.CultureInfo.InvariantCulture) +
+                   " + " + Tax.ToString("F2", System.Globalization.CultureInfo.InvariantCulture) +
+                   " = " + TotalPayment.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Interfaces/Services/RentalService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces.Entities;
+
+namespace Interfaces.Services
+{
+    internal class RentalService
+    {
+        public double pricePerHouer { get; private set; }
+        public double pricePerDay { get; private set; }
+
+        public RentalService(double pricePerHouer, double pricePerDay)
+        {
+            this.pricePerHouer = pricePerHouer;
+            this.pricePerDay = pricePerDay;
+        }
+
+        public void processInvoice(CarRental) 
+        {
+
+        }
+    }
+}
