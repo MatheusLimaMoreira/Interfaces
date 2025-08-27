e3fa680128a3c12c4355ed8b665153c89f2d7352
Matheus Moreira <matheus.moreira127@gmail.com>
2025-08-27 13:20:39 -0300

    Primeiros passo exercicio de fixação

---
+++ ExercicioFixacao/Entities/Contract.cs
@@ -0,0 +1,16 @@
+﻿namespace ExercicioFixacao.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateOnly Date { get; set; }
+        public double TotalValue { get; set; }
+
+        public Contract(int number, DateOnly date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+    }
+}
---
+++ ExercicioFixacao/Entities/Installments.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.VisualBasic;
+
+namespace ExercicioFixacao.Entities
+{
+    internal class Installments
+    {
+        public  DueDate Date { get; set; }
+        public double Amount { get; set; }
+
+        public Installments(DueDate date, double amount)
+        {
+            Date = date;
+            Amount = amount;
+        }
+    }
+}
